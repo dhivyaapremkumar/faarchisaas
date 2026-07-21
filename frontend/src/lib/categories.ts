20985635ffc27ba8796f976901d699825d483b1a
dhivyaapremkumar <dhivu23scorpio@gmail.com>
2026-07-21 12:10:01 +0530

    Add files via upload

---
+++ frontend/src/lib/categories.ts
@@ -0,0 +1,2 @@
+export const TEAM_CATEGORIES = ["Architect", "Client", "Structural", "Electrical", "Plumbing", "A/C", "Others"];
+export const VENDOR_CATEGORIES = ["Structural", "Electrical", "Plumbing", "A/C", "Others"];
